609715a24d31211440719381aae6861d6ccfd08a
Doodie223 <duc.nh.927@aptechlearning.edu.vn>
2024-05-30 00:19:31 +0700

    ok

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+
       minlength: 6,
       maxlength: 30,
       unique: true,
@@ -19,7 +20,7 @@ const userSchema = new mongoose.Schema(
       minlength: 6,
     },
   },
-    { timestamps: true }
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", userSchema);
